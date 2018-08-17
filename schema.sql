b81ac87958b85b8f40d8bafc4faa2f057ea7ee2f
Roderick Trice <rocky_trice@yahoo.com>
2018-08-17 17:11:02 -0400

    initial data added to database. have to populate the rest of the data

--- schema.sql
+++ schema.sql
@@ -6,10 +6,10 @@ USE bamazon;
 
 /* Create new table with a primary key that auto-increments, and a text field */
 CREATE TABLE products (
-  item_id INT NOT NULL,
+  item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
   product_name VARCHAR(100) NOT NULL,
   department_name VARCHAR(100) NOT NULL,    
-  price DECIMAL(10,4) NULL,
+  price DECIMAL(10,2) NULL,
   stock_quantity INTEGER(10),
   PRIMARY KEY (item_id)
 );
---
+++ seeds.sql
@@ -0,0 +1,12 @@
+USE bamazon;
+
+INSERT INTO products (item_id,product_name,department_name,price,stock_quantity)
+values(1,'NBA 2K19','Electronics','59.96','250');
+
+
+
+
+
+
+ /* ("NBA 2K19"),("Fallout 76"),("Far Cry 5"),("Overwatch - Game of the Year Edition"),("Shadow of the Tomb Raider (Croft Steelbook Edition)"),("Tom Clancy’s Ghost Recon Wildlands"),("The Elder Scrolls V: Skyrim Special Edition"),("Destiny 2"),("Monster Hunter: World"),("Madden 19"); */
+ 
