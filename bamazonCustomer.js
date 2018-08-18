7a2ab4d959713b8bab4763bab19b12b26dc51bc4
Roderick Trice <rocky_trice@yahoo.com>
2018-08-18 01:12:45 -0400

    question prompts are added in

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,17 +1,42 @@
-
 var mysql = require("mysql");
 
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
- host: "localhost",
- port: 8889,
- user: "root",
- password: "root",
-database: "bamazon"
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "bamazon"
 })
-connection.connect(function(err) {
+// connecting to bamazon database
+connection.connect(function (err) {
     if (err) throw err;
     console.log('connected as id ' + connection.threadId);
-    connection.end();
-  });
+    customerOrder();
+});
+
+
+
+
+
+// questions for ordering a product
+function customerOrder() {
+    inquirer
+        .prompt([
+            {
+                name: "order",
+                type: "input",
+                message: "What's the  ID of the product  would you like to buy?"
+            },
+            {
+                name: "buy",
+                type: "input",
+                message: "How many units would you like to buy?"
+            }
+
+        ])
+        
+}
+
+
